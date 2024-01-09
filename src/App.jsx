6dc5d616de73502594837cff2e85e6e275a91a79
chisomA22 <akanichisom@gmailcom>
2024-01-09 12:01:13 +0100

    cloud app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const App = () =>{
   return (
     <div>
-      
+        <Navbar/> 
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { IoClose } from "react-icons/io5";
+import { IoMenuSharp } from "react-icons/io5";
 
 const  Navbar = ()=> {
   return (
     <div>
-      
+      <h1>Navbar</h1>
     </div>
   )
 }
