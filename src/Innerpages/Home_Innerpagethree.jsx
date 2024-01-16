2eb6e5d0736e58d1067ab86682cff9bd68c07acd
chisomA22 <akanichisom@gmailcom>
2024-01-16 12:01:11 +0100

    cloud app

---
+++ src/Innerpages/Home_Innerpagethree.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaSquarePhoneFlip } from "react-icons/fa6"
+import { IoHardwareChipSharp } from 'react-icons/io5'
+
+const Home_Innerpagethree=()=> {
+  return (
+    <div className=''>
+  <FaSquarePhoneFlip size={25}/> <IoHardwareChipSharp size={25}/>
+    </div>
+  )
+}
+
+export default Home_Innerpagethree
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home_Innerpage from '../Innerpages/Home_Innerpage'
+import Home_Innerpagetwo from '../Innerpages/Home_Innerpagetwo'
+import Home_Innerpagethree from '../Innerpages/Home_Innerpagethree'
 
 const Home=()=> {
   return (
     <div>
       <Home_Innerpage/>
+      <Home_Innerpagetwo/>
+      <Home_Innerpagethree/>
     </div>
   )
 }
